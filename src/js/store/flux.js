3302a296d296bd3aa7e672a35917d5458b971aba
Andres Calfulef <rcalfulef@gmail.com>
2019-12-19 09:43:33 -0300

    cambios

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			apiUrl: "http://best-health.ddns.net:8001",
+			apiUrl: "http://0.0.0.0:8000",
 			apiUrl2: "https://apy-cors-fcobad.herokuapp.com/https://mobile.bestdoctorsinsurance.com/spiritapi/api",
 			token: {
 				refresh: "",
